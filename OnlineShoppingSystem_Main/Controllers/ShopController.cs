7eb23f8a10588e1098877e5c1f29339299a870a5
Ducanh2605 <anhndhe180984@fpt.edu.vn>
2025-02-24 23:58:37 +0700

    Update Shop have product but missing filter and div css

---
+++ OnlineShoppingSystem_Main/Controllers/ShopController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingSystem_Main.Data.Models;
+using OnlineShoppingSystem_Main.Models;
+
+namespace OnlineShoppingSystem_Main.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Swd392OssContext _context;
+
+        public ProductController(Swd392OssContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+
+            var products = _context.Products
+                                   .Include(p => p.Category)
+                                   .Include(p => p.ProductImages)
+                                   .ToList();
+
+            ViewBag.Categories = categories;
+
+            return View(products); 
+        }
+
+      
+
+
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products
+                                 .Include(p => p.Category)
+                                 .Include(p => p.ProductImages)
+                                 .FirstOrDefault(p => p.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}

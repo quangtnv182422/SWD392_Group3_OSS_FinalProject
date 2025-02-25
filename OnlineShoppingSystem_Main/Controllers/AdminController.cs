6bb9cb64668af5b15dc7cc5c405adf3ab79afd43
Ducanh2605 <anhndhe180984@fpt.edu.vn>
2025-02-26 00:14:04 +0700

    Update Admin dashboard

---
+++ OnlineShoppingSystem_Main/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShoppingSystem_Main.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

9cf1f2d009f301267277d54214815179a9395e31
Ducanh2605 <anhndhe180984@fpt.edu.vn>
2025-02-27 01:20:25 +0700

    Add config for cloudinary config and package , Update ProductAdmin Dashboard (still missing controller for edit , view , add , delete )

---
+++ OnlineShoppingSystem_Main/Controllers/AdminProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingSystem_Main.Data.Models;
+using OnlineShoppingSystem_Main.Models;
+using System.Linq;
+
+namespace OnlineShoppingSystem_Main.Controllers
+{
+    public class AdminProductController : Controller
+    {
+        private readonly Swd392OssContext _context;
+
+        public AdminProductController(Swd392OssContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ProductList(int page = 1, int pageSize = 10)
+        {
+            var products = _context.Products
+                                   .Include(p => p.ProductStatus) 
+                                   .Skip((page - 1) * pageSize)
+                                   .Take(pageSize)
+                                   .ToList();
+
+            var totalCount = _context.Products.Count();
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            ViewBag.CurrentPage = page;
+
+            return View(products);
+        }
+
+
+       
+    }
+}
